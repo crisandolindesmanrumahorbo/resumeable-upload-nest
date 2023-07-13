92776e5d0cb57351173786c2f5508718e10561f4
Crisandolin Rumahorbo <crisandolin.rumahorbo@btpn.com>
2023-07-13 17:52:41 +0700

    handle sub folder

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,13 +1,36 @@
-import { All, Controller, Req, Res } from '@nestjs/common';
-import { FileService } from './file.service';
+import {
+  Controller,
+  Delete,
+  Head,
+  Patch,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
+import { FileService } from './file.service';
 
 @Controller('files/*')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
-  @All()
+  @Patch()
   async upload(@Req() request: Request, @Res() response: Response) {
-    this.fileService.handleUpload(request, response);
+    return this.fileService.handle(request, response);
+  }
+
+  @Post()
+  async initialize(@Req() request: Request, @Res() response: Response) {
+    return this.fileService.handleInitiate(request, response);
+  }
+
+  @Head()
+  async checkChunk(@Req() request: Request, @Res() response: Response) {
+    return this.fileService.handle(request, response);
+  }
+
+  @Delete()
+  async delete(@Req() request: Request, @Res() response: Response) {
+    return this.fileService.handle(request, response);
   }
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -2,21 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { FileStore } from '@tus/file-store';
 import { Metadata, Server } from '@tus/server';
 import { Request, Response } from 'express';
+import { existsSync, mkdirSync } from 'fs';
 
 @Injectable()
 export class FileService {
+  getMetadata = (req: Request) => {
+    const { 'upload-metadata': uploadMetadata } = req.headers;
+    return Metadata.parse(uploadMetadata as string);
+  };
+
   private readonly tusServer = new Server({
     path: '/files/upload',
     datastore: new FileStore({ directory: './storage' }),
     namingFunction: (req: Request) => {
-      // 'upload-metadata': 'filename YmVyYmVyYmVyLm00YQ==,filetype YXVkaW8veC1tNGE=',
-      const { 'upload-metadata': uploadMetadata } = req.headers;
-      const { filename } = Metadata.parse(uploadMetadata as string);
-      return filename;
+      const { roomCode, fileType, filename } = this.getMetadata(req);
+      return `${roomCode}/${fileType}/${filename}`;
     },
   });
 
-  async handleUpload(request: Request, response: Response) {
+  async handle(request: Request, response: Response) {
+    return this.tusServer.handle(request, response);
+  }
+
+  async handleInitiate(request: Request, response: Response) {
+    const { roomCode, fileType } = this.getMetadata(request);
+    const dir = `/Users/20058748/Documents/personal/twilio/tus.io/nest-server/storage/${roomCode}/${fileType}/`;
+    if (!existsSync(dir)) {
+      mkdirSync(dir, { recursive: true });
+    }
     return this.tusServer.handle(request, response);
   }
 }
